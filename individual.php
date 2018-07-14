af44860a2e4a34eca12a42bb52d793947cb9ed74
csravya333 <36538690+csravya333@users.noreply.github.com>
2018-07-14 17:55:48 +0530

    Individual_signup-php page

---
+++ individual.php
@@ -0,0 +1,49 @@
+<?php
+	$link = mysqli_connect('localhost', 'root' , '', 'jpmc');
+	if(!$link){
+die('error connection');
+}
+	$name = ''; 
+if( isset( $_POST['name'])) {
+    $name = $_POST['name']; 
+} 
+$email = ''; 
+if( isset( $_POST['email'])) {
+    $email = $_POST['email']; 
+} 
+$gender = ''; 
+if( isset( $_POST['gender'])) {
+    $gender = $_POST['gender']; 
+} 
+$age = ''; 
+if( isset( $_POST['age'])) {
+    $ = $_POST['age']; 
+} 
+$colgname = ''; 
+if( isset( $_POST['colgname'])) {
+    $colgname = $_POST['colgname']; 
+} 
+$disability = ''; 
+if( isset( $_POST['disability'])) {
+    $disability = $_POST['disability']; 
+}
+$mobile = ''; 
+if( isset( $_POST['mobile'])) {
+    $mobile = $_POST['mobile']; 
+}
+$password = ''; 
+if( isset( $_POST['password'])) {
+    $password = $_POST['password']; 
+}
+
+$query = "select * from `university_signup` where colgname='$colgname' ";
+$result = mysqli_query($link,$query);
+if (mysqli_num_rows($result) > 0) {
+	echo "University has already registered!"
+	echo "<br> Click here to <a href='individual_signup.html'> login </a>";
+}
+else{
+	$con="INSERT INTO `individual_signup`(name,email,gender,age,colgname,disability,mobile,password) VALUES ('$name', '$email','$gender','$age','$colgname', '$disability' ,'$mobile','$password' )";
+}
+	mysqli_close($link);
+?>

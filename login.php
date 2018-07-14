268e3766e612bdfe13779bb57db78bfa245bb6d9
csravya333 <36538690+csravya333@users.noreply.github.com>
2018-07-15 02:33:16 +0530

    Modified login.php,individual.php,login.html

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 
 
 <?php
-$link = mysqli_connect('localhost', 'root' , '', 'test');
+$link = mysqli_connect('localhost', 'root' , '', 'jpmc');
 if(!$link){
 	die('error connection');
 }
@@ -19,16 +19,16 @@ if( isset( $_POST['password'])) {
     $password = $_POST['password']; 
 } 
 
-$query = "select * from `user_register` where email='$email' and password='$password' ";
+$query = "select * from `individual_signup` where email='$email' and password='$password' ";
+echo "select * from `individual_signup` where email='$email' and password='$password'";
 $result = mysqli_query($link,$query);
 
 if (mysqli_num_rows($result) > 0) {
-//	echo "entered";
+ // echo "entered";
 	$_SESSION["email"] = "$email";
+//	echo "$email";
 //	echo  "hi" .$_SESSION["email"]. "!";
 	header("Location: dashboard_individual.html");
 }
-else {
-	echo "Wrong password or wrong username <br> Click her to<a href='user_login.html'>login</a>";
-}
+
 ?>
